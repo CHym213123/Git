bc1cbbcbf7e06689040ade2a29efa67c19db2d5b
CHym213123 <luccahu0003@gmail.com>
2025-03-04 15:28:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function showProduct(id) {
+function showProduct(id) { chym
+                          
     var products = document.querySelectorAll('.product-detail');
     products.forEach(product => product.classList.remove('active'));
     document.getElementById(id).classList.add('active');
